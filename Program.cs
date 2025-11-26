5dd9c0bc92081d6ef09865f8daf4408e0479460f
reutne <zenasustov7@gmail.com>
2025-11-26 17:59:27 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace LibraryApp
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
